d10fa7ed40e2d809178808a0d01f7e164cc6fde3
naut <jrbarnachea@gmail.com>
2022-05-23 11:51:26 -0700

    Update forms.py

--- forms.py
+++ forms.py
@@ -4,5 +4,5 @@ from wtforms.validators import DataRequired, URL
 
 #WTForms
 class UploadForm(FlaskForm):
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    img_url = StringField("Image URL", validators=[DataRequired(), URL()])
     submit = SubmitField("Upload Image")
