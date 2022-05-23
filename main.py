d10d6ead76694e3d0ee2e97f515c510a2b696f0c
naut <jrbarnachea@gmail.com>
2022-05-23 09:16:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,19 @@ from collections import Counter
 import PIL
 
 
+def show_img_and_comparison(img, img_2):
+    """Displays 2 images in comparison (Image x Top Colors). 1 row, 2 columns."""
+
+    # create the layout
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(20,20))
+
+    # adjust the display properties
+    ax[0].imshow(img)
+    ax[1].imshow(img_2)
+    ax[0].axis('off')   # hide the axis
+    ax[1].axis('off')   # hide the axis
+
+    fig.tight_layout()
+    plt.show()
+
+
