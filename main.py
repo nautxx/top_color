cf7283273d3f7271af6d352896fdeeac9c718fdf
naut <jrbarnachea@gmail.com>
2022-05-23 09:16:17 -0700

    Update main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import cv2 as cv    # pip install opencv-python
+import numpy as np  # pip install numpy
+import matplotlib.pyplot as plt # pip install matplotlib
+from sklearn.cluster import KMeans
+from collections import Counter
+import PIL
+
+
