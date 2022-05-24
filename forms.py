033fdda8985f96e18368096eb4f9aaafa55a0517
naut <jrbarnachea@gmail.com>
2022-05-23 21:40:33 -0700

    keep project CLI.

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, URL
-
-#WTForms
-class UploadForm(FlaskForm):
-    img_url = StringField("Image URL", validators=[DataRequired(), URL()])
-    submit = SubmitField("Upload Image")
